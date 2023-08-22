c09921aedcba5e341255947a4fbbefe335150497
Flipuk <69309833+LeoNchiC@users.noreply.github.com>
2023-08-22 06:23:42 +0300

    GameList

--- GameList.py
+++ GameList.py
@@ -1,4 +1,9 @@
 import pickle
+from colorama import init, Fore, Style
+import random
+
+
+init()
 
 class Game:
     def __init__(self, title):
@@ -12,44 +17,51 @@ class GameList:
     def add_game(self, title):
         game = Game(title)
         self.games.append(game)
+        print(Fore.GREEN + f"Игра '{title}' добавлена в список."+ Style.RESET_ALL)
 
     def mark_completed(self, title):
         for game in self.games:
             if game.title == title:
                 game.completed = True
-                print(f"Игра '{title}' отмечена как пройденная.")
+                print(Fore.GREEN + f"Игра '{title}' отмечена как пройденная."+ Style.RESET_ALL)
                 return
-        print(f"Игра '{title}' не найдена в списке.")
+        print(Fore.RED + f"Игра '{title}' не найдена в списке." + Style.RESET_ALL)
 
     def remove_game(self, title):
         for game in self.games:
             if game.title == title:
                 self.games.remove(game)
-                print(f"Игра '{title}' удалена из списка.")
+                print(Fore.LIGHTYELLOW_EX + f"Игра '{title}' удалена из списка." + Style.RESET_ALL)
                 return
-        print(f"Игра '{title}' не найдена в списке.")
+        print(Fore.RED + f"Игра '{title}' не найдена в списке." + Style.RESET_ALL)
+
+    def get_random_uncompleted_game(self):
+        uncompleted_games = [game for game in self.games if not game.completed]
+        if not uncompleted_games:
+            return None  
+        return random.choice(uncompleted_games)
 
     def display_list(self):
         if not self.games:
-            print("Список игр пуст.")
+            print(Fore.LIGHTBLACK_EX + "Список игр пуст." + Style.RESET_ALL)
         else:
-            print("Список игр:")
+            print(Fore.YELLOW + "Список игр:" + Style.RESET_ALL)
             for game in self.games:
-                status = "Пройдено" if game.completed else "Не пройдено"
+                status = Fore.LIGHTGREEN_EX + "Пройдено" + Style.RESET_ALL if game.completed else Fore.LIGHTRED_EX +  "Не пройдено" + Style.RESET_ALL
                 print(f"- {game.title} ({status})")
 
     def save_to_file(self, filename):
         with open(filename, "wb") as file:
             pickle.dump(self.games, file)
-        print("Список игр сохранен.")
+        print(Fore.GREEN + "Список игр сохранен." + Style.RESET_ALL)
 
     def load_from_file(self, filename):
         try:
             with open(filename, "rb") as file:
                 self.games = pickle.load(file)
-            print("Список игр загружен.")
+            print(Fore.GREEN + "Список игр загружен." + Style.RESET_ALL)
         except FileNotFoundError:
-            print("Файл со списком игр не найден.")
+            print(Fore.RED + "Файл со списком игр не найден." + Style.RESET_ALL)
 
 def main():
     game_list = GameList()
@@ -58,32 +70,44 @@ def main():
     game_list.load_from_file(filename)
 
     while True:
-        print("\nВыберите действие:")
+        print(Fore.BLUE + "\nВыберите действие:" + Style.RESET_ALL)
         print("1. Добавить игру")
         print("2. Отметить игру как пройденную")
         print("3. Показать список игр")
         print("4. Удалить игру из списка")
-        print("5. Сохранить и выйти")
+        print("5. Выбрать рандомную игру")
+        print("6. Сохранить и выйти")
 
-        choice = input("Введите номер действия: ")
+        choice = input(Fore.BLUE + "Введите номер действия: " + Style.RESET_ALL)
 
         if choice == "1":
             title = input("Введите название игры: ")
             game_list.add_game(title)
+
         elif choice == "2":
             title = input("Введите название игры, которую хотите отметить как пройденную: ")
             game_list.mark_completed(title)
+
         elif choice == "3":
             game_list.display_list()
+
         elif choice == "4":
             title = input("Введите название игры, которую хотите удалить: ")
             game_list.remove_game(title)
+
         elif choice == "5":
+            uncompleted_game = game_list.get_random_uncompleted_game()
+            if uncompleted_game:
+                print(Fore.CYAN + "Выбрана случайная не пройденная игра: " + Style.RESET_ALL + Fore.MAGENTA + f"{uncompleted_game.title}" + Style.RESET_ALL)
+            else:
+                print(Fore.YELLOW + "Все игры пройдены или список пуст." + Style.RESET_ALL)
+
+        elif choice == "6":
             game_list.save_to_file(filename)
-            print("Программа завершена.")
+            print(Fore.MAGENTA + "Программа завершена." + Style.RESET_ALL)
             break
         else:
-            print("Некорректный ввод. Пожалуйста, выберите действие из списка.")
+            print(Fore.RED + "Некорректный ввод. Пожалуйста, выберите действие из списка." + Style.RESET_ALL)
 
 if __name__ == "__main__":
     main()
